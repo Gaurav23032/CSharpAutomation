f7ff83d3cacefeb1c86f5ebf5d52411f63d3cbd7
gauravsk <gauravksagar2020@gmail.com>
2022-12-26 18:19:07 +0530

    Programs

--- Employeeproject/Employee.cs
+++ Employeeproject/Employee.cs
@@ -12,16 +12,64 @@ namespace Employeeproject
         public string empName;
         public double empSalary;
         public static string companyName;
-        public  static string companyLocation;
+        public static string companyLocation;
+        public char empPerformanceType;
+
+        public double EmpSalary
+        {
+            get
+            {
+                return empSalary;
+            }
+            set
+            {
+                if(value>3000)
+                {
+                    empSalary = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Salary So Default value remains !!!");
+                }
+            }
+        }
 
         public void printEmployeeDetails()
         {
             Console.WriteLine("Employee Id:" + empId);
             Console.WriteLine("Employee Name:" + empName);
             Console.WriteLine("Employee Salary:" + empSalary);
-            Console.WriteLine("company Name:" +Employee.companyName);
-            Console.WriteLine("Employee Location:"+Employee.companyLocation);
+            Console.WriteLine("company Name:" + Employee.companyName);
+            Console.WriteLine("Employee Location:" + Employee.companyLocation);
+            Console.WriteLine("Employee Performance:" + empPerformanceType);
             Console.WriteLine("-------------------------------------------");
+        }
+        //calculate Bonus
+
+        public void GetGrossSalaryWithBonus()
+        {
+
+            if (empPerformanceType == 'A')
+            {
+                Console.WriteLine("25%");
+                Console.WriteLine(empSalary + (empSalary * 25/ 100));
+            }
+            else if (empPerformanceType == 'B')
+            {
+                Console.WriteLine("15%");
+                Console.WriteLine(empSalary + (empSalary * 15/ 100));
+            }
+            else if (empPerformanceType == 'c')
+            {
+                Console.WriteLine("10%");
+                Console.WriteLine(empSalary + (empSalary * 10/ 100));
+            }
+            else
+            {
+                Console.WriteLine("Not Eligible for Bonus");
+            }
+
+
         }
     }
     
--- Employeeproject/Program.cs
+++ Employeeproject/Program.cs
@@ -18,15 +18,20 @@ namespace Maveric.Runner
 
             emp1.empId = 101;
             emp1.empName = "saul";
-            emp1.empSalary = 9000;
+            emp1.empSalary = -9000;
+            emp1.empPerformanceType = 'B';
+           
 
             emp2.empId = 102;
             emp2.empName = "kim";
             emp2.empSalary = 12000.2;
+            emp1.empPerformanceType = 'A';
 
             emp3.empId = 103;
             emp3.empName = "jack";
             emp3.empSalary = 6000.2;
+            emp1.empPerformanceType ='c';
+           
 
           //  emp3.empId = 104;
           //  emp3.empName = "Gaurav";
@@ -38,6 +43,10 @@ namespace Maveric.Runner
             emp2.printEmployeeDetails();
             emp3.printEmployeeDetails();
             emp4.printEmployeeDetails();
+            emp1.GetGrossSalaryWithBonus();
+            emp2.GetGrossSalaryWithBonus();
+            emp3.GetGrossSalaryWithBonus();
+            emp4.GetGrossSalaryWithBonus();
         }
     }
 }
