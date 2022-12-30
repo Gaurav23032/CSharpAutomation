b7f3816cc4da78e003b577443b01ac877861a31f
gauravsk <gauravksagar2020@gmail.com>
2022-12-30 10:05:16 +0530

    29 day assignment

--- GoToMeeting/Program.cs
+++ GoToMeeting/Program.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 
 namespace GoToMeeting
 {
@@ -12,7 +14,24 @@ namespace GoToMeeting
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
             driver.FindElement(By.XPath("//button[@type='button']")).Click();
-           driver.FindElement(By.LinkText("")).Click();
+            Actions actions = new Actions(driver);
+            try
+            {
+                actions.ScrollToElement(driver.FindElement(By.LinkText("Try Free"))).Perform();
+            }
+            catch { }
+            Thread.Sleep(5000);
+           driver.FindElement(By.LinkText("Try Free")).Click();
+
+            driver.FindElement(By.Name("FirstName")).SendKeys("john");
+            driver.FindElement(By.Name("LastName")).SendKeys("wick");
+            driver.FindElement(By.XPath("//input[@id='login__email']")).SendKeys("john@gmail.com");
+            
+            SelectElement select = new SelectElement(driver.FindElement(By.XPath("//select[@id='CompanySize']")));
+            select.SelectByValue("10 - 99");
+            driver.FindElement(By.XPath("//button[text()='Start My Trial']")).Click();
+
+            
         }
     }
 }
---
+++ SeleniumAdavance/MultipleTabsCitiTest.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumAdavance
+{
+    public  class MultipleTabsCitiTest
+    {
+        [Test]
+        public void MultiCity()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "https://www.online.citibank.co.in/";
+            driver.FindElement(By.XPath("//a[@title='Close']")).Click();
+            driver.FindElement(By.XPath("//span[text()='Login']")).Click();
+            Thread.Sleep(2000);
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+            driver.FindElement(By.XPath("//div[@onclick='ForgotUserID();']")).Click();
+            driver.FindElement(By.LinkText("select your product type")).Click();
+            driver.FindElement(By.LinkText("Credit Card")).Click();
+            driver.FindElement(By.Id("citiCard1")).SendKeys("4545");
+            driver.FindElement(By.Id("citiCard2")).SendKeys("5656");
+            driver.FindElement(By.Id("citiCard3")).SendKeys("8887");
+            driver.FindElement(By.Id("citiCard4")).SendKeys("9998");
+            driver.FindElement(By.Id("cvvnumber")).SendKeys("123");
+            driver.FindElement(By.Id("bill-date-long")).Click();
+            SelectElement month = new SelectElement(driver.FindElement(By.XPath("//select[@class='ui-datepicker-month']")));
+            month.SelectByText("Apr");
+            SelectElement year = new SelectElement(driver.FindElement(By.XPath("//select[@class='ui-datepicker-year']")));
+            year.SelectByText("2022");
+            driver.FindElement(By.LinkText("14")).Click();
+            driver.FindElement(By.XPath("//input[@type='button']")).Click();
+            Thread.Sleep(1000);
+            string textPrint = driver.FindElement(By.XPath("//li[contains(text(),'• Please accept Terms and Conditions ')]")).Text;
+            Console.WriteLine(textPrint);
+
+
+
+
+
+
+
+        }
+    }
+}
